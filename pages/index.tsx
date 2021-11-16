903799cd4c92fa6a543d2b8c5176ca3de537ceda
Skeleton Praxis <skeletonpraxis@pm.me>
2021-11-15 18:20:06 -0600

    implemented stitches, styled header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,31 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import * as SeparatorPrimitive from '@radix-ui/react-separator'
+import { styled } from '../stitches.config'
+
+const StyledSeparator = styled(SeparatorPrimitive.Root, {
+	backgroundColor: '#656661',
+	'&[data-orientation=horizontal]': { height: 2, width: '100%' },
+	'&[data-orientation=vertical]': { height: '100%', width: 2 }
+})
+
+export const Header = styled('div', {
+	display: 'flex'
+})
+export const HeaderTextContent = styled('div', {
+	display: 'flex',
+	flexDirection: 'column',
+	marginLeft: '50px',
+	justifyContent: 'end',
+	'& h1': {
+		margin: 0
+	}
+})
+export const Separator = StyledSeparator
+export const Text = styled('p', {
+	margin: 0
+})
 
 const Home: NextPage = () => {
 	return (
@@ -11,13 +36,32 @@ const Home: NextPage = () => {
 				<link rel='icon' href={'/favicon.ico'} />
 			</Head>
 
-			<div>
-				<Image src='/HomeSkeleton.jpg' width={140} height={240} />
-				<p>
-					thi;asdf;sajlsaojwef psjfsdpsd i spojpispfsdpoi do
-					dpisdospijwefjao{' '}
-				</p>
-			</div>
+			<Header>
+				<Image src='/HomeSkeleton.jpg' width={140} height={240} alt={'skeleton logo'} />
+				<HeaderTextContent>
+					<h1>Skeleton Praxis</h1>
+					<Separator
+						decorative
+						orientation='horizontal'
+						css={{ margin: '15px 0' }}
+					/>
+					<div style={{ display: 'flex' }}>
+						<Text>Games</Text>
+						<Separator
+							decorative
+							orientation='vertical'
+							css={{ margin: '0 15px' }}
+						/>
+						<Text>Blog</Text>
+						<Separator
+							decorative
+							orientation='vertical'
+							css={{ margin: '0 15px' }}
+						/>
+						<Text>About</Text>
+					</div>
+				</HeaderTextContent>
+			</Header>
 		</div>
 	)
 }
