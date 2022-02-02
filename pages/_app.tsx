214c5b71e6d310b3b039b98925a2b2006c5faa31
Skeleton Praxis <skeletonpraxis@pm.me>
2022-02-01 18:39:29 -0600

    remove dinos day temp post, clean up meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,35 +8,54 @@ function SkeletonPraxisHome({ Component, pageProps }: AppProps) {
 		<div>
 			<Head>
 				<meta charSet='UTF-8' />
+				<title>Skeleton Praxis</title>
 				<meta
 					name='viewport'
 					content='width=device-width, initial-scale=1'
 				/>
-				<title>Skeleton Praxis</title>
 				<link rel='icon' href={'/favicon.ico'} />
 				<link rel='icon' href={'/favicon.svg'} type='image/svg+xml' />
 				<meta
 					name='description'
-					content='Skeleton Praxis Game Development Studio'
+					content='Skeleton Praxis Game Studio Homepage'
 				/>
+				<link rel="shortcut icon" href={'/favicon.ico'} />
 
+				{/*Facebook OpenGraph meta tags*/}
 				<meta property='og:title' content='Skeleton Praxis' />
+				<meta property='og:type' content='website' />
+				<meta
+					property='og:url'
+					content='https://www.skeletonpraxis.com/'
+				/>
+				<meta
+					property='og:image'
+					content='https://skeletonpraxis.com/HomeSkeleton.jpg'
+				/>
 				<meta
 					property='og:description'
-					content='Skeleton Praxis Game Development Studio'
+					content='Skeleton Praxis Game Studio Homepage'
 				/>
-				<meta property='og:image' content='HomeSkeleton.jpg' />
 				<meta
 					property='og:image:alt'
 					content='Skeleton Praxis Mascot'
 				/>
-				<meta property='og:locale' content='en_GB' />
-				<meta property='og:type' content='website' />
-				<meta name='twitter:card' content='summary' />
+				<meta property="og:site_name" content="Skeleton Praxis" />
+
+				{/*Twitter meta tags*/}
+				<meta name='twitter:title' content='Skeleton Praxis' />
 				<meta
-					property='og:url'
-					content='https://www.mywebsite.com/page'
+					name='twitter:description'
+					content='Skeleton Praxis Game Studio Homepage'
 				/>
+				<meta
+					name='twitter:image'
+					content='https://skeletonpraxis.com/HomeSkeleton.jpg'
+				/>
+				<meta name='twitter:card' content='summary' />
+				<meta name='twitter:creator' content='@SkeletonPraxis' />
+				<meta name='twitter:site' content='@SkeletonPraxis' />
+
 				<link rel='canonical' href='https://www.skeletonpraxis.com/' />
 
 				<link rel='apple-touch-icon' href={'/skull.png'} />
